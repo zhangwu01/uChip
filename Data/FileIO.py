158f83f0dc8c0eed9536a05b85a023ffbd1797d1
“Zhang” <zhangwu@g.harvard.edu>
2025-12-08 18:12:50 -0600

    modified FileIO.py to Fix cross-platform dill unpickling: redirect WindowsPath to PosixPath and simplify LoadObject/SaveObject

--- Data/FileIO.py
+++ Data/FileIO.py
@@ -1,15 +1,36 @@
 import dill
-from pathlib import Path
+# from pathlib import Path
+from pathlib import PosixPath
 
 
-def LoadObject(path: Path):
-    file = open(path, "rb")
-    obj = dill.load(file)
-    file.close()
-    return obj
+class CrossPlatformUnpickler(dill.Unpickler):
+    def find_class(self, module, name):
+        # Redirect WindowsPath to PosixPath on macOS/Linux
+        if module == 'pathlib' and name == 'WindowsPath':
+            return PosixPath
+        return super().find_class(module, name)
+    
+
+# def LoadObject(path: Path):
+#     file = open(path, "rb")
+#     obj = dill.load(file)
+#     file.close()
+#     return obj
 
 
-def SaveObject(obj, path: Path):
-    file = open(path, "wb+")
-    dill.dump(obj, file)
-    file.close()
+# def SaveObject(obj, path: Path):
+#     file = open(path, "wb+")
+#     dill.dump(obj, file)
+#     file.close()
+
+
+def LoadObject(file_path):
+    """Load a dill/pickle object from file, fixing WindowsPath on non-Windows systems."""
+    with open(file_path, 'rb') as file:
+        return CrossPlatformUnpickler(file).load()
+
+
+def SaveObject(obj, file_path):
+    """Save a dill/pickle object to file."""
+    with open(file_path, 'wb') as file:
+        dill.dump(obj, file)
